07638f86632e47e98e8ecd562a15f0c01276e839
adrianagraybill <adriana.graybill@gmail.com>
2020-05-20 21:59:43 -0700

    delete button styled and functional

--- client/src/components/listUsers.jsx
+++ client/src/components/listUsers.jsx
@@ -5,6 +5,17 @@ const ListUsers = () => {
 
   const [users, setUsers] = useState([]);
 
+  const deleteUser = async id => {
+    try {
+      const deleteUser = await fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE"
+      });
+      setUsers(users.filter(user => user.user_id !== id));
+    } catch (err) {
+      console.log(err.mesagge);
+    }
+  }
+
   const getUsers = async () => {
     try {
       const response = await fetch("http://localhost:8000/users")
@@ -27,18 +38,31 @@ const ListUsers = () => {
           <td class="tdWide">Name</td>
           <td class="tdWide">Email</td>
           <td class="tdWide">Phone Number</td>
-          <td></td>
-          <td></td>
+          <td class="tdNarrow"></td>
+          <td class="tdNarrow"></td>
         </tr>
       </thead>
       <tbody>
-        {users.map(user => (
-          <tr>
-            <td class="tdWide">{user.user_name}</td>
-            <td class="tdWide">{user.user_email}</td>
-            <td class="tdWide">{user.user_phone}</td>
-            <td>Edit</td>
-            <td>Delete</td>
+        {users.map(users => (
+          <tr key={users.user_id}>
+            <td class="tdWide">{users.user_name}</td>
+            <td class="tdWide">{users.user_email}</td>
+            <td class="tdWide">{users.user_phone}</td>
+            <td>
+              <button 
+                class="btn"
+                id="editBtn">Edit
+              </button>
+            </td>
+            <td>
+              <button 
+                class="btn" 
+                id="deleteBtn" 
+                onClick={() => deleteUser(users.user_id)}
+              > 
+                Delete
+              </button>
+            </td>
           </tr>
         ))}
       </tbody>
